5698fd80a7512f5c22fb469bfe856919a2850de9
diskomimfo <nasyayhudin1@gmail.com>
2024-11-17 21:57:53 +0700

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.getElementById('hamburger');
-    const dataList = document.querySelector('.data-list');
+    const hamburger = document.getElementById('hamburger'); // Mendapatkan elemen tombol hamburger
+    const dataList = document.querySelector('.data-list'); // Mendapatkan elemen menu dropdown
 
+    // Menambahkan event listener untuk menangani klik pada tombol hamburger
     hamburger.addEventListener('click', () => {
         dataList.classList.toggle('active'); // Toggle kelas active pada data-list
-        // Tambahkan animasi atau perubahan gaya jika ingin
-        hamburger.classList.toggle('is-active'); // Menambahkan kelas aktif pada hamburger jika diperlukan
     });
 });
