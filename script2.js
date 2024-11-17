00f201f1339877fc762bce8b9c60f85e6b2713f1
diskomimfo <nasyayhudin1@gmail.com>
2024-11-17 11:25:33 +0700

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,9 @@
+<script>
+    const hamburger = document.getElementById('hamburger');
+    const dataList = document.querySelector('.data-list');
+
+    hamburger.addEventListener('click', ()  
+        dataList.classList.toggle('active'); // Toggle kelas active pada data-list
+    
+    );
+</script>
